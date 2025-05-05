65cf9ebcb98b1260933e262a1f16ab27ff85ae9d
July Jairetsell León Salcedo <130617019+JulyLeonJ@users.noreply.github.com>
2025-05-05 01:54:41 -0500

    Delete loginowasp/securelogin directory

--- loginowasp/securelogin/autentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('password_reset/', views.password_reset_view, name='password_reset'),
-    path('registro/', views.registro_view, name='registro'),
-]
